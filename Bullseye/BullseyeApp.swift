491994a1aafda616b99f848543bd225167f50a3a
Andrey1224 <nepod77@gmail.com>
2021-07-15 17:45:15 -0400

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Andrii Nepodymka on 7/15/21.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
