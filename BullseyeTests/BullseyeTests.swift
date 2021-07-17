5c003ddd857a68f54e59026c1c43ecb2d4f3639e
Andrey1224 <nepod77@gmail.com>
2021-07-16 20:03:12 -0400

    keep working

---
+++ BullseyeTests/BullseyeTests.swift
@@ -0,0 +1,46 @@
+//
+//  BullseyeTests.swift
+//  BullseyeTests
+//
+//  Created by Andrii Nepodymka on 7/16/21.
+//
+
+import XCTest
+@testable import Bullseye
+
+class BullseyeTests: XCTestCase {
+    
+    
+    var game: Game!
+
+    override func setUpWithError() throws {
+        game = Game()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        game = nil
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testScorePositive() {
+        
+        let guess = game.target + 5
+        let score = game.points(sliderValue: guess)
+        XCTAssertEqual(score, 95)
+        
+    }
+    
+    func testScoreNegative() {
+        
+        let guess = game.target - 5
+        let score = game.points(sliderValue: guess)
+        XCTAssertEqual(score, 95)
+        
+    }
+    
+    
+
+
+
+}
